c9d9acb3a68c6ea58b06bb3618aca4ea5ab1e113
TortelloSte <s.perdicchia@icloud.com>
2023-07-04 01:36:34 +0200

    modifiche e aggiunta dile con ML e DL

--- machine_learning.py
+++ machine_learning.py
@@ -5,19 +5,42 @@ import os
 
 df = pd.read_csv('./data/new_heart.csv')
 
-# ora che i dati sono stati corretti, andiamo a esplorarli in maniera approfondita
+# ora che i dati sono stati corretti, andiamo ad applicare qualche algoritmo di classificazione
 
-sns.countplot(x="target", data=df, palette="bwr")
-plt.savefig(os.path.join('./graph/grafici_ML', 'target.png'))
+# 1: RANDOM FOREST
 
-no = len(df[df.target == 0])
-yes = len(df[df.target == 1])
-print("Percentuale dei pazienti senza malattia cardiaca: {:.2f}%".format((no / len(df.target)) * 100))
-print("Percentuale dei pazienti con malattia cardiaca: {:.2f}%".format((yes / len(df.target)) * 100))
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 
-sns.countplot(x='sex', data=df, palette="mako_r")
-plt.xlabel("Sex (0 = female, 1= male)")
-plt.show()
+X = df.drop("target", axis=1)
+y = df["target"]
 
-print(df['sex'].value_counts())
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+rf = RandomForestClassifier()
+
+# Addestra il modello sulla porzione di training set
+rf.fit(X_train, y_train)
+
+# Esegui le previsioni sul test set
+predictions = rf.predict(X_test)
+
+# Calcola l'accuratezza del modello
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
+
+
+cm = confusion_matrix(y_test, predictions)
+
+# Crea un grafico della matrice di confusione
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=["No malattia", "Malattia"], yticklabels=["No malattia", "Malattia"])
+plt.xlabel("Previsto")
+plt.ylabel("Effettivo")
+plt.title("Matrice di Confusione")
+plt.savefig(os.path.join('./graph/grafici_ML', 'grafico_matrice_confuzione.png'))
+plt.close()
+
